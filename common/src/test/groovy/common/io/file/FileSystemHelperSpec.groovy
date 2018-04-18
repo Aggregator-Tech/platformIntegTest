e1e9976edd3474275ac289a0a375690e770b5dc7
Ajeet Bansal <ajeetbansal@gmail.com>
2018-04-18 13:18:45 -0700

    Multi project gradle build with spock based tests

---
+++ common/src/test/groovy/common/io/file/FileSystemHelperSpec.groovy
@@ -0,0 +1,10 @@
+package common.io.file
+
+import spock.lang.Specification
+
+class FileSystemHelperSpec extends Specification {
+    def 'Test getFile()'() {
+        setup:
+        FileSystemHelper fileSystemHelper = new FileSystemHelper();
+    }
+}
